d1f4eb5713d4c63b363bcdc9b44ff5a7030a309d
Bart Opsomer <bart.opsomer@sioux.eu>
2015-12-11 08:53:05 +0100

    save

---
+++ testRandom/testRandom.h
@@ -0,0 +1,33 @@
+/*
+ * testRandom.h
+ *
+ *  Created on: Dec 10, 2015
+ *      Author: student
+ */
+
+#ifndef TESTRANDOM_H_
+#define TESTRANDOM_H_
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+//#include <time.h>
+#include <sys/time.h>
+#include <math.h>
+#include <linux/random.h>
+#include <sys/syscall.h>
+#include <signal.h>
+//#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <linux/random.h>
+
+int main(void);
+
+
+
+#endif /* TESTRANDOM_H_ */
